91f3fb6ccb3efbd48f822ceef4a4a29ad2e71356
JadhavAnsh <jadhavansh10@gmail.com>
2025-07-18 22:39:56 +0530

    feat(ui): Added the Shadcn components and next-themes for the dark mode functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -12,6 +13,9 @@ export default function Home() {
           height={38}
           priority
         />
+        <Button>
+          DevDigest AI
+        </Button>
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
